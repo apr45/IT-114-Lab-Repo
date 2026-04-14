38d8d3d0f34e7a26936456f29cfe438f448696bd
Alex <apr45@njit.edu>
2026-04-14 17:51:07 -0400

    Initial commit: Adding code and screenshot from Lab 01 and gitignore file

---
+++ Lab01_Setup/Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+    public static void main (String[] args){
+        //prints "Hello World"
+        System.out.println("Hello World");
+
+        //prints current Java version
+        System.out.println("Running Java version: " + System.getProperty("java.version"));
+    }
+}
